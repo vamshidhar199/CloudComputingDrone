58f987e5404c854c019c1ba11471a35dac71074e
Gajam-Anurag <89657709+Gajam-Anurag@users.noreply.github.com>
2022-11-03 19:29:38 -0700

    Latest changes

--- client/src/Admin/AdminEditDrone/Admineditdrone.js
+++ client/src/Admin/AdminEditDrone/Admineditdrone.js
@@ -4,11 +4,11 @@ import React from "react";
 
 
 
-function AdminaddDrone(){
+function AdmineditDrone(){
     return <>
     <Box mt = {20}  spacing={2} ml={6} width='60%' >
-        <Typography variant="h4" textAlign='left'>Add a New Drone</Typography>
-        <Typography textAlign='left'>Register this drone into the cloud system so he drone is Available to book</Typography>
+        <Typography variant="h4" textAlign='left'>Edit Drone</Typography>
+        <Typography textAlign='left'>Edit details of selected drone</Typography>
         <Box sx={{p:4, width:'100%' ,border: '1px solid grey', backgroundColor:'white'}}>
             <Typography sx={{textAlign:'left', marginBottom:'2%'}}>Drone Information</Typography>
             <Stack spacing={2}>
@@ -24,4 +24,4 @@ function AdminaddDrone(){
     </>
 }
 
-export default AdminaddDrone;
+export default AdmineditDrone;
